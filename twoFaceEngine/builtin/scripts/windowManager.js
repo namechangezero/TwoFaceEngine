430088917403d7b21dc45de0473e7bc5d83c3820
namechangezero <alessandrokrauter@gmail.com>
2023-09-23 01:50:19 +0200

    fixed bug of sprite duplication for no reason

--- twoFaceEngine/builtin/scripts/windowManager.js
+++ twoFaceEngine/builtin/scripts/windowManager.js
@@ -2,8 +2,10 @@
 (function(){
 
     window.addEventListener("resize", () => {
-        canvas.width = window.innerWidth
-        canvas.height = window.innerHeight
+        canvas_size = {"x":window.innerWidth - 20, "y":window.innerHeight - 20}
+        
+        canvas.width = canvas_size.x
+        canvas.height = canvas_size.y
     })
 
 })()
