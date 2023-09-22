5e34a7dfa48462af2c6d1dc92a8ba54347110209
namechangezero <alessandrokrauter@gmail.com>
2023-09-23 01:36:13 +0200

    added gamesettings framerate setting functionality

--- twoFaceEngine/builtin/scripts/GameConfig.js
+++ twoFaceEngine/builtin/scripts/GameConfig.js
@@ -10,9 +10,14 @@ class GameSettingsClass{
         document.title = title;
     }
 
+    setFPS(fps){
+        Time.waitAfterFrame = 1000/fps;
+    }
+
     loadGameSettingsJson(){
         fetch('../../../GameSettings.json').then(response => response.json()).then(json => {
             this.setGameTitle(json.GameWindowTitle);
+            this.setFPS(json.default_framerate);
         })
 
     }
